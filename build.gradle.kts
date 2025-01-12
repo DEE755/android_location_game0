ecb0823bca3507ef63c67bf500c98d6e0f2cd6d4
Dorian <dorian.atlan755@hotmail.fr>
2025-01-12 22:36:46 +0200

    add

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,20 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Top-level build.gradle file for Gradle 7.0+ using plugins DSL
+buildscript {
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.0.0") // Ensure it's compatible with Gradle 8.0+
+    }
+}
+
+
+repositories {
+    google()
+    mavenCentral()
+}
+
 plugins {
     alias(libs.plugins.android.application) apply false
+    //id("com.google.gms.google-services") version "4.3.15" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
+
+
